2e2ff2141493ea46f71f2a77444ef8d5a22da3d1
Jonaira Manha <abcd@Jonaeath.local>
2024-07-25 21:02:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+
+app.get("/", async (req,res)=>{
+    res.send("Test server is running");
+});
+
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`));
+
+
+
+
+
